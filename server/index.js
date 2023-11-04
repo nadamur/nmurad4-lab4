6187fdac53aaa7cf80cb433f148e91f62c96895f
nadamur <nmurad4@uwo.ca>
2023-11-04 00:58:52 -0400

    method 2 finished

--- server/index.js
+++ server/index.js
@@ -11,7 +11,20 @@ app.use(express.json());
 app.get('/api/superheroes/:id', (req, res) => {
     const superheroId = parseInt(req.params.id);
     const superhero = superheroesInfo.find((hero) => hero.id === superheroId);
-    const superheroPowers = superheroPowersData[superheroName];
+  
+    if (!superhero) {
+      return res.status(404).json({ error: 'Superhero not found' });
+    }
+  
+    res.json(superhero);
+});
+
+//this method will return the powers of hero based on id
+app.get('/api/superheroes/:id/power', (req, res) => {
+    const superheroId = parseInt(req.params.id);
+    const superhero = superheroesInfo.find((hero) => hero.id === superheroId);
+    const superheroPowers = superheroesPowers.find((hero) => hero.hero_names === superhero.name);
+    console.log(superheroPowers)
   
     if (!superhero) {
       return res.status(404).json({ error: 'Superhero not found' });
@@ -19,26 +32,14 @@ app.get('/api/superheroes/:id', (req, res) => {
 
     const powers = Object.keys(superheroPowers).filter(power => superheroPowers[power] === 'True');
 
-    if (truePowers.length > 0) {
-      res.json({ truePowers });
+    if (powers.length > 0) {
+      res.json({ powers });
     } else {
       res.status(404).json({ error: 'No true powers found for this superhero' });
     }
-  
-    res.json(superhero);
 });
 
-//this method will return the powers of the given hero's id
-app.get('/api/superheroes/:id/power', (req, res) => {
-    const superheroId = parseInt(req.params.id);
-    const superhero = superheroesInfo.find((hero) => hero.id === superheroId);
-  
-    if (!superhero) {
-      return res.status(404).json({ error: 'Superhero not found' });
-    }
-  
-    res.json(superhero);
-});
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
