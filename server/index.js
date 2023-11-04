41d0445e1c6477143f4748ce55d7f3315611faac
nadamur <nmurad4@uwo.ca>
2023-11-04 00:48:39 -0400

    return id method finished

--- server/index.js
+++ server/index.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const superheroesInfo = require('./superhero_info.json');
+const superheroesInfo = require('C:/se 3316/lab 3/se3316-nmurad4-lab3/superhero_info.json');
+const superheroesPowers = require('C:/se 3316/lab 3/se3316-nmurad4-lab3/superhero_powers.json');
+
 
 app.use(express.json());
 
-app.get('/',(req,res) => {
-    res.send('Hello');
+//this method will return all the hero info depending on id
+app.get('/api/superheroes/:id', (req, res) => {
+    const superheroId = parseInt(req.params.id);
+    const superhero = superheroesInfo.find((hero) => hero.id === superheroId);
+    const superheroPowers = superheroPowersData[superheroName];
+  
+    if (!superhero) {
+      return res.status(404).json({ error: 'Superhero not found' });
+    }
+
+    const powers = Object.keys(superheroPowers).filter(power => superheroPowers[power] === 'True');
+
+    if (truePowers.length > 0) {
+      res.json({ truePowers });
+    } else {
+      res.status(404).json({ error: 'No true powers found for this superhero' });
+    }
+  
+    res.json(superhero);
 });
 
-app.get('/api/superheroes/:id', (req, res) => {
+//this method will return the powers of the given hero's id
+app.get('/api/superheroes/:id/power', (req, res) => {
     const superheroId = parseInt(req.params.id);
     const superhero = superheroesInfo.find((hero) => hero.id === superheroId);
   
